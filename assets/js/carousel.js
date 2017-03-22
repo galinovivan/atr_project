0f0ed3fffc3bc348af5456ce47a0fd5a84bd43b7
U-Web-master\master <C:\Users\master\AppData\Roaming\The Bat!>
2017-03-22 17:30:07 +0300

    add slider

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -4,9 +4,11 @@
        var slider = $('#centerSlider');
 
         slider.slick({
-            centerPadding: '60px',
+           centerPadding: '0px',
             centerMode: true,
-            slidesToShow: 5
+            slidesToShow: 5,
+            prevArrow: '<div class="arrow left"><i class="social_arrow social_arrow-arrow_left"></i></div>',
+            nextArrow: '<div class="arrow right"><i class="social_arrow social_arrow-arrow_right"></i></div>',
         });
     });
 
