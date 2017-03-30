5195575181fa731165ce51f02c1831c4b45d7f48
galinovivan <9225913@gmail.com>
2017-03-30 18:23:23 +0300

    nomination

---
+++ nomination.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: master
+ * Date: 30.03.2017
+ * Time: 18:00
+ * Template name: Номинация
+ */
+?>
+
+<?php get_header('custom'); ?>
+<div class="category_heading">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4 class="up white"><?php wp_title('');?></h4>
+            </div>
+        </div>
+    </div>
+</div>
+<main class="nomination_page">
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="sub_heading">
+                        <h3 class="black"><?=get_field('subheader');?></h3>
+                </div>
+                    <div class="text">
+                        <?php if (have_posts()):
+                            while(have_posts()):
+                                the_post(); ?>
+                                <p class="black"><?=get_the_content();?></p>
+                                <?php
+                            endwhile;
+                        endif;
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 submit_row">
+                    <a href="<?=get_field('template_link');?>" target="_blank" class="button base black medium">
+                        Скачать шаблон
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php get_footer(); ?>
+
+
+
+
