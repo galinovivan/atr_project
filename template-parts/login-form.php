19c8b1cadd412ea748a7b5be45a3eb217e693227
galinovivan <9225913@gmail.com>
2017-04-05 14:06:43 +0300

    купАщкь

--- template-parts/login-form.php
+++ template-parts/login-form.php
@@ -10,27 +10,28 @@ if (is_user_logged_in()):
             чтобы мы вас узнали.</p>
     </div>
     <div class="login_form">
-        <form class="base_form userform" name="loginform" id="loginform" method="post" action="">
-            <div class="row">
-                <div class="col-md-12 field_item">
-                    <input type="email" name="auth_email" placeholder="Email" />
-                </div>
-                <div class="col-md-12 field_item">
-                    <input type="password" name="auth_pass" placeholder="Пароль" />
-                </div>
-                <div class="col-md-12 checkbox_field">
-                    <input type="checkbox" name="rememberme" id="authRemember" value="forever"/>
-                    <label for="authRemember">Запомнить меня</label>
-                </div>
-                <div class="submit_field">
-                    <input type="submit" class="button base medium black block_center" value="Войти" />
-                </div>
-                <input type="hidden" name="redrect_to" value="<?=$_SERVER['REQUEST_URI'];?>" />
-                <input type="hidden" name="nonce" value="<?=wp_create_nonce('login_me_nonce');?>" />
-                <input type="hidden" name="action" value="login_me"/>
-                <div class="response response_message"></div>
-            </div>
-        </form>
+<!--        <form class="base_form userform" name="loginform" id="loginform" method="post" action="">-->
+<!--            <div class="row">-->
+<!--                <div class="col-md-12 field_item">-->
+<!--                    <input type="email" name="auth_email" placeholder="Email" />-->
+<!--                </div>-->
+<!--                <div class="col-md-12 field_item">-->
+<!--                    <input type="password" name="auth_pass" placeholder="Пароль" />-->
+<!--                </div>-->
+<!--                <div class="col-md-12 checkbox_field">-->
+<!--                    <input type="checkbox" name="rememberme" id="authRemember" value="forever"/>-->
+<!--                    <label for="authRemember">Запомнить меня</label>-->
+<!--                </div>-->
+<!--                <div class="submit_field">-->
+<!--                    <input type="submit" class="button base medium black block_center" value="Войти" />-->
+<!--                </div>-->
+<!--                <input type="hidden" name="redrect_to" value="--><?//=$_SERVER['REQUEST_URI'];?><!--" />-->
+<!--                <input type="hidden" name="nonce" value="--><?//=wp_create_nonce('login_me_nonce');?><!--" />-->
+<!--                <input type="hidden" name="action" value="login_me"/>-->
+<!--                <div class="response response_message"></div>-->
+<!--            </div>-->
+<!--        </form>-->
+        <?=do_shortcode('[wpuf-login]');?>
     </div>
 </div>
 <?php endif; ?>
