2264c830c223fedee32a70e2aee409a8a6087f91
galinovivan <9225913@gmail.com>
2017-04-12 17:09:45 +0300

    if

--- header-custom.php
+++ header-custom.php
@@ -153,11 +153,19 @@
     </div>
 </div>
 <a href="#" class="top_link">Наверх</a>
-<div class="helper red">
-    <span>Приём работ открыт с 1 апреля до 15 сентября 2017. Чтобы принять участие, нажмите  <a href="#registrationModal" data-toggle="modal"> здесь</a>.</span>
+<div class="helper red" id="topHelper">
+    <?php if (!is_user_logged_in()): ?>
+        <span>Приём работ открыт с 1 апреля до 15 сентября 2017. Чтобы принять участие, нажмите
+        <a href="#registrationModal" data-toggle="modal"> здесь</a>.</span>
+        <a class="helper_close" href="#">
+            <span></span><span></span>
+        </a>
+    <?php else: ?>
+    <span>Приём работ открыт с 1 апреля до 15 сентября 2017. Чтобы принять участие, нажмите
+        <a href="<?=get_page_link(68);?>" data-toggle="modal"> здесь</a>.</span>
     <a class="helper_close" href="#">
         <span></span><span></span>
-    </a>
+        <?php endif; ?>
 </div>
 <header class="light_header">
     <div class="container">
