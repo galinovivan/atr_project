0efb752dbd79e4347f39058de33826569a89f7de
galinovivan <9225913@gmail.com>
2017-04-24 17:55:21 +0300

    adaptive template

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <div class="participation_banner light_background">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-7 about wrapper fluid_wrapper">
+                <div class="hidden-xs hidden-sm col-md-7 about wrapper fluid_wrapper">
                     <div class="banner_heading with_separate">
                         <h2 class="up black">Условия участия</h2>
                     </div>
@@ -48,9 +48,36 @@
                         <a href="<?=get_page_link(469);?>" target="_blank" class="black button base medium ">Смотреть</a>
                     </div>
                 </div>
-                <div class="col-md-5 thumb_wrapper">
+                <div class="col-xs-12 col-md-5 thumb_wrapper">
                     <div class="thumb">
-                        <!--<img src="assets/images/people_banner-2.png"  class="img-responsive" />-->
+                       <h2 class="up white text-center">Условия участия</h2>
+                        <div class="bottom_separate_line"></div>
+                    </div>
+                </div>
+                <div class="col-xs-12 visible-xs visible-sm about_wrapper">
+                    <div class="banner_heading with_separate">
+                        <h2 class="up black">Условия участия</h2>
+                    </div>
+                    <div class="text">
+                        <p class="black">В конкурсе могут принимать участие студенты, бакалавры, магистры,
+                            аспиранты, учащиеся и выпускники 2011-2017 гг. высших учебных заведений РФ,
+                            а также средних профессиональных образовательных учреждений старше 16 лет.
+                            Как индивидуальные авторы, так и творческие коллективы до 3 человек.</p>
+                    </div>
+                    <div class="more_text">
+                        <div class="row">
+                            <div class="col-md-1">
+                                <img src="<?=get_template_directory_uri()?>/assets/images/pencil_icon.png" class="img-responsive" />
+                            </div>
+                            <div class="col-md-11 text_right">
+                                <span class="small_text black">Студенты и выпускники соревнуются в разных категориях.
+В каждой из трёх номинаций для студентов присуждается три призовых места.
+В каждой из трёх номинаций для выпускников присуждается по одному призовому месту.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="button_more">
+                        <a href="<?=get_page_link(469);?>" target="_blank" class="black button base medium ">Смотреть</a>
                     </div>
                 </div>
             </div>
