8b46bef0593d3db6d2c9d42341d3a94041b7751a
galinovivan <9225913@gmail.com>
2017-04-28 17:37:24 +0300

    s

--- header-primary.php
+++ header-primary.php
@@ -7,8 +7,8 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
-    <meta name="viewport" content="width=1280">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--    <meta name="viewport" content="width=1280">-->
 
     <link rel="shortcut icon" href="<?=get_template_directory_uri();?>/assets/images/fav.ico" type="image/ico">
     <script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = 'https://vk.com/rtrg?p=VK-RTRG-106461-5F5Ub';</script>
@@ -22,7 +22,7 @@
         <img src="<?=get_template_directory_uri();?>/assets/images/preloader.gif" />
     </div>
 </div>
-<div class="helper red" id="topHelper">
+<div class="helper red hidden-xs hidden-sm" id="topHelper">
     <?php if (!is_user_logged_in()): ?>
         <span>Приём работ открыт с 1 апреля до 15 сентября 2017. Чтобы принять участие, нажмите
         <a href="#registrationModal" data-toggle="modal"> здесь</a>.</span>
@@ -36,23 +36,35 @@
         <span></span><span></span>
         <?php endif; ?>
 </div>
+<div class="helper red visible-xs visible-sm">
+    <span class="white">С 1 апреля до 15 сентября 2017 идёт приём работ.</span>
+</div>
 
 <header class="base_background primary_header_container">
     <div class="container">
         <div class="row top_header_section">
-            <div class="col-md-6">
+            <div class="col-xs-6 col-md-6">
                 <div class="header_logo text-left">
                     <a href="/">
-                        <img src="<?=get_template_directory_uri()?>/assets/images/arh_logo.png" class="img-responsive" />
+                        <img src="<?=get_template_directory_uri()?>/assets/images/arh_logo.png"
+                             class="img-responsive hidden-xs hidden-sm" />
+                        <img src="<?=get_template_directory_uri();?>/assets/images/mobile_arh_logo.png"
+                             class="visible-xs visible-sm" />
                     </a>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-xs-6 col-md-6">
                 <div class="header_logo text-right clearfix">
                     <a href="http://www.cds.spb.ru" target="_blank">
-                        <i class="pull-right logo_header_icon"
+                        <i class="pull-right logo_header_icon hidden-xs hidden-sm"
                            title="Учредитель и организатор конкурса"  data-toggle="tooltip" data-placement="bottom">
-                        </i> </a>
+                        </i>
+                        <div class="mobile_block_cds visible-xs visible-sm">
+                            <h5 class="white">Учредитель и организатор:</h5>
+                            <img src="<?=get_template_directory_uri();?>/assets/images/mobile_cds_logo.png"
+                                 class="img-responsive" />
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
