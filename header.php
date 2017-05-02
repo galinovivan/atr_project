b617686b9851892f2c35f7e1e93159e74b0560b6
galinovivan <9225913@gmail.com>
2017-05-02 16:14:50 +0300

    в

--- header.php
+++ header.php
@@ -186,8 +186,8 @@
             </div>
             <div class="col-md-3 left_separate">
                 <ul class="nav">
-                    <li><a href="<?=get_page_link(26)?>" class="up white">Работы<span class="base_font"> (Студенты)</span></a></li>
-                    <li><a href="<?=get_page_link(26);?>" class="up white">Работы<span class="base_font"> (Выпускники)</span></a></li>
+                    <li><a href="<?=get_page_link(190)?>" class="up white">Работы<span class="base_font"> (Студенты)</span></a></li>
+                    <li><a href="<?=get_page_link(186);?>" class="up white">Работы<span class="base_font"> (Выпускники)</span></a></li>
                     <li><a href="<?=get_page_link(188);?>" class="up white">Финалисты 2016</a></li>
                     <li><a href="<?php is_user_logged_in() ? print get_page_link(68) : print "#registrationModal" ?>" class="up white gold_marker" data-toggle="modal">Участвовать</a></li>
                 </ul>
