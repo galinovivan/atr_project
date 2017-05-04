c979a5fcb65375aa3837b7d6ced1118cdbb5fedc
galinovivan <9225913@gmail.com>
2017-05-04 13:14:08 +0300

    вы

--- about.php
+++ about.php
@@ -37,26 +37,26 @@
             </div>
         </div>
     </div>
-<div class="shedule hidden-xs hidden-sm">
+<div class="shedule hidden-xs">
     <div class="date_row">
         <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 col-sm-6">
                 <div class="date bottom_separate orange">
                     <span class="black">1.04.17</span>
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-2 col-sm-2">
                 <div class="date">
                     <span class="black bold_font">15.09.17</span>
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-2 col-sm-2">
                 <div class="date">
                     <span class="black">25.09.17</span>
                 </div>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-2 col-sm-2">
                 <div class="date">
                     <span class="black">02.10.17</span>
                 </div>
@@ -70,7 +70,7 @@
     <div class="action_row">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 left_separate black">
+                <div class="col-md-6 col-sm-6 left_separate black">
                     <div class="action">
                         <p class="black">
                             Старт<br />
@@ -79,7 +79,7 @@
                         </p>
                     </div>
                 </div>
-                <div class="col-md-2 left_separate black">
+                <div class="col-md-2 col-sm-2 left_separate black">
                     <div class="action">
                         <p class="black bold_font">
                             Окончание<br />
@@ -88,7 +88,7 @@
                         </p>
                     </div>
                 </div>
-                <div class="col-md-2 left_separate black">
+                <div class="col-md-2 col-sm-2 left_separate black">
                     <div class="action">
                         <p class="black">
                             Обьявление<br />
@@ -97,7 +97,7 @@
                         </p>
                     </div>
                 </div>
-                <div class="col-md-2 left_separate black">
+                <div class="col-md-2 col-sm-2 left_separate black">
                     <div class="action small_padding">
                         <p class="black">
                             Выставка<br />
@@ -113,7 +113,7 @@
     </div>
 
 </div>
-    <div class="visible-xs visible-sm shedule">
+    <div class="visible-xs shedule hidden-sm hidden-md hidden-lg">
         <div class="container">
             <div class="row">
                <div class="col-xs-12 action_item active">
