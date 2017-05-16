060008f9ea7e163ad8aa38ea46db0685a6bd53c9
galinovivan <9225913@gmail.com>
2017-05-16 16:17:24 +0300

    sd

--- profile.php
+++ profile.php
@@ -28,7 +28,10 @@
             </div>
         </div>
     </div>
+
     <div class="container">
+        <div class="alert alert-success" id="userMessages" style="display: none;">
+        </div>
         <div class="row">
             <div class="col-xs-12 col-md-8">
     <div class="hel_block">
@@ -37,12 +40,14 @@
         <div class="text">
             <?php
             $user = wp_get_current_user();
+            $userUrl = $user->user_url;
             ?>
             <h5>Здравствуйте, <?=$user->user_lastname;?> </h5>
             <p class="black">
                 Здесь вы можете загрузить свою работу для участия во Всероссийском профессиональном студенческом конкурсе
                 архитектурных и дизайнерских проектов «АРХпроект».
             </p>
+
         </div>
         <div class="down_row">
             <a href="<?=get_page_link(68);?>" class="button base medium black">Загрузить работу</a>
@@ -55,12 +60,25 @@
                     <div class="info_list">
                         <ul>
                             <li class="black">
-
-                               Дата регистрации на сайте:
+                                Дата регистрации на сайте:
                                 <span class="black data"><?=getUserRegistered($user->ID);?></span>
                             </li>
 
                         </ul>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-5">
+                        <form id="updateUserInfo" class="base_form update_user_form" method="post"
+                              action="<?php echo esc_url(admin_url('admin-post.php'));?>">
+                            <input type="hidden" value="update_user" name="action" id="formAction" />
+                            <input type="hidden" value="<?=$user->ID;?>" name="user_id" id="userId" />
+                            <label for="userUrl">Я в социальных сетях</label>
+                            <input type="text" placeholder="Ссылка на профиль в социальных сетях"
+                                   value="<?=$userUrl;?>" name="user_url" id="userUrl" />
+                            <button class="button base transparent medium black">Сохранить</button>
+                        </form>
                     </div>
                 </div>
 <!--    <div class="actions_list">-->
