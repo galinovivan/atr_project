78f1f367307df12351719bbfa889d9d33da3c21e
galinovivan <9225913@gmail.com>
2017-05-17 16:06:42 +0300

    s

--- assets/js/like.js
+++ assets/js/like.js
@@ -1,40 +1,41 @@
 /**
  * Created by master on 17.05.2017.
  */
-(function( $ ) {
+// (function( $ ) {
+//
+//     $(document).ready(function() {
+//
+//         filterLikeCounterText();
+//
+//         var $likeBtn = $('wp_ulike_btn');
+//
+//         $likeBtn.on('click', function() {
+//            filterLikeCounterText();
+//         });
+//
+//
+//
+//
+//
+//         function filterLikeCounterText() {
+//             var $counterBox = $('.counter .count-box');
+//
+//             var currentNumber = filterStringToInteger($counterBox.text());
+//
+//             $counterBox.text(currentNumber);
+//         }
+//
+//         /**
+//          * @param string
+//          * @returns {Number}
+//          */
+//         function filterStringToInteger(string) {
+//             return parseInt(string);
+//         }
+//
+//
+//     });
+//
+//
+// })(jQuery);
 
-    $(document).ready(function() {
-
-        filterLikeCounterText();
-
-        var $likeBtn = $('wp_ulike_btn');
-
-        $likeBtn.on('click', function() {
-           filterLikeCounterText();
-        });
-
-
-
-
-
-        function filterLikeCounterText() {
-            var $counterBox = $('.counter .count-box');
-
-            var currentNumber = filterStringToInteger($counterBox.text());
-
-            $counterBox.text(currentNumber);
-        }
-
-        /**
-         * @param string
-         * @returns {Number}
-         */
-        function filterStringToInteger(string) {
-            return parseInt(string);
-        }
-
-
-    });
-
-
-})(jQuery);
