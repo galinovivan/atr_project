18352d223c178ce7ee37a7ef17837ab360ace5e4
galinovivan <9225913@gmail.com>
2017-05-19 12:24:19 +0300

    add nav template

---
+++ template-parts/nav.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: master
+ * Date: 19.05.2017
+ * Time: 12:00
+ */
+?>
+
+<div class="main_nav" id="mainNav">
+    <a class="nav_close" href="#">
+        <span></span><span></span>
+    </a>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 left_separate">
+                <ul class="nav">
+                    <li><a href="<?=get_page_link(29);?>" class="up white"><?=__('[:ru]О конкурсе[:en]about');?></a></li>
+                    <li><a href="<?=get_page_link(175);?>" class="up white"><?=__('[:ru]Новости[:en]news');?></a></li>
+                    <li><a href="<?=get_page_link(40)?>" class="up white"><?=__('[:ru]Жюри[:en]Judge');?></a></li>
+                    <li><a href="<?=get_page_link(56)?>" class="up white"><?=__('[:ru]Партнеры[:en]Partners');?></a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 left_separate">
+                <ul class="nav">
+                    <li class="sub_menu not_hover"><span class="sub sub_padd">
+                            <?=__('[:ru]Номинации[:en]Nomination');?></span>
+                        <ul class="sub_nav">
+                            <li><a href="<?=get_page_link(115);?>" target="_blank">
+                                    <?=__('[:ru]Интерьер квартиры[:en]Interior of apartment');?>
+                                </a></li>
+                            <li><a href="<?=get_page_link(165);?>" target="_blank">
+<?__('[:ru]Павильон/беседка[:en]nothing');?></a></li>
+                            <li><a href="<?=get_page_link(168);?>" target="_blank">
+                                    <?=__('[:ru]Остановка транспорта[:en]Bus station');?>
+                                </a></li>
+                            <li><a href="<?=get_page_link(525);?>" target="_blank">
+                                    <?=__('[:ru]Город на воде[:en]City on the water');?>
+                                </a></li>
+                        </ul>
+                    </li>
+                    <li><a href="<?=get_page_link(171);?>" class="up white">
+                            <?=__('[:ru]Призы[:en]Prizez');?>
+                        </a></li>
+                    <li><a href="<?=get_page_link(469);?>" class="up white">Условия участия</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 left_separate">
+                <ul class="nav">
+                    <li><a href="<?=get_page_link(190)?>" class="up white">Работы<span class="base_font"> (Студенты)</span></a></li>
+                    <li><a href="<?=get_page_link(186);?>" class="up white">Работы<span class="base_font"> (Выпускники)</span></a></li>
+                    <li><a href="<?=get_page_link(188);?>" class="up white">Финалисты 2016</a></li>
+                    <li><a href="<?php is_user_logged_in() ? print get_page_link(68) : print "#registrationModal" ?>"
+                           class="up white gold_marker" data-toggle="modal">
+                            <?php is_user_logged_in() ? print "Загрузить работу" : print "Участвовать" ?>
+                        </a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 left_separate">
+                <ul class="nav">
+                    <li>
+
+                        <?php if (is_user_logged_in()): ?>
+                            <a href="<?=get_page_link(177);?>" class="up white">Личный кабинет</a>
+                        <?php else: ?>
+                            <a href="#registrationModal" data-toggle="modal" class="up white">Регистрация</a>
+                        <?php endif; ?>
+
+                    </li>
+                    <li><a href="#contactForm" class="up white scroll_link">Задать вопрос</a></li>
+                    <li><a href="#contactForm" class="up white scroll_link">Контакты</a></li>
+                    <li>
+                        <div class="social_block">
+                            <ul>
+                                <li><a href="https://www.facebook.com/pro.zvezdaudachi/" target="_blank">
+                                        <i class="soc_hov_new soc_hov_new-fb"></i>
+                                    </a></li>
+                                <li><a href="https://vk.com/pro.zvezdaudachi" target="_blank">
+                                        <i class="soc_hov_new soc_hov_new-vk"></i>
+                                    </a></li>
+                                <li><a href="http://instagram.com/pro.zvezdaudachi" target="_blank">
+                                        <i class="soc_hov_new soc_hov_new-inst"></i>
+                                    </a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
