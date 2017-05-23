a89e4146bd4d0dfd8da9ece1f44a8d077d86916c
galinovivan <9225913@gmail.com>
2017-05-23 12:34:28 +0300

    add translate

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -30,7 +30,7 @@
                                     <?=__('[:ru]Интерьер квартиры[:en]Interior of apartment');?>
                                 </a></li>
                             <li><a href="<?=get_page_link(165);?>" target="_blank">
-<?__('[:ru]Павильон/беседка[:en]nothing');?></a></li>
+<?=__('[:ru]Павильон/беседка[:en]nothing');?></a></li>
                             <li><a href="<?=get_page_link(168);?>" target="_blank">
                                     <?=__('[:ru]Остановка транспорта[:en]Bus station');?>
                                 </a></li>
@@ -42,17 +42,33 @@
                     <li><a href="<?=get_page_link(171);?>" class="up white">
                             <?=__('[:ru]Призы[:en]Prizez');?>
                         </a></li>
-                    <li><a href="<?=get_page_link(469);?>" class="up white">Условия участия</a></li>
+                    <li><a href="<?=get_page_link(469);?>" class="up white">
+                            <?=__('[:ru]Условия участия[:en]The terms of partication');?>
+                        </a></li>
                 </ul>
             </div>
             <div class="col-md-3 left_separate">
                 <ul class="nav">
-                    <li><a href="<?=get_page_link(190)?>" class="up white">Работы<span class="base_font"> (Студенты)</span></a></li>
-                    <li><a href="<?=get_page_link(186);?>" class="up white">Работы<span class="base_font"> (Выпускники)</span></a></li>
-                    <li><a href="<?=get_page_link(188);?>" class="up white">Финалисты 2016</a></li>
+                    <li><a href="<?=get_page_link(190)?>" class="up white">
+                            <?=__('[:ru]Работы[:en]Projects');?>
+                            <span class="base_font">
+                                <?=__('[:ru](Студенты)[:en](Students]');?>
+                            </span>
+                        </a></li>
+                    <li><a href="<?=get_page_link(186);?>" class="up white">
+                            <?=__('[:ru]Работы[:en]Projects')?>
+                            <span class="base_font">
+                                <?=__('[:ru](Выпускники)[:en]Graduates');?>
+                            </span></a></li>
+                    <li><a href="<?=get_page_link(188);?>" class="up white">
+                            <?=__('[:ru]Финалисты 2016[:en]The finalists');?>
+                        </a></li>
                     <li><a href="<?php is_user_logged_in() ? print get_page_link(68) : print "#registrationModal" ?>"
                            class="up white gold_marker" data-toggle="modal">
-                            <?php is_user_logged_in() ? print "Загрузить работу" : print "Участвовать" ?>
+                            <?php
+                            is_user_logged_in()
+                                ? print __('[:ru]Загрузить работу[:en]Download the work') :
+                                print __('[:ru]Участвовать[:en]Participate'); ?>
                         </a></li>
                 </ul>
             </div>
@@ -61,14 +77,22 @@
                     <li>
 
                         <?php if (is_user_logged_in()): ?>
-                            <a href="<?=get_page_link(177);?>" class="up white">Личный кабинет</a>
+                            <a href="<?=get_page_link(177);?>" class="up white">
+                                <?=__('[:ru]Личный кабинет[:en]profile');?>
+                            </a>
                         <?php else: ?>
-                            <a href="#registrationModal" data-toggle="modal" class="up white">Регистрация</a>
+                            <a href="#registrationModal" data-toggle="modal" class="up white">
+                                <?=('[:ru]Регистрация[:en]Registration');?>
+                            </a>
                         <?php endif; ?>
 
                     </li>
-                    <li><a href="#contactForm" class="up white scroll_link">Задать вопрос</a></li>
-                    <li><a href="#contactForm" class="up white scroll_link">Контакты</a></li>
+                    <li><a href="#contactForm" class="up white scroll_link">
+                            <?=__('[:ru]Задать вопрос[:en]Ask questions');?>
+                        </a></li>
+                    <li><a href="#contactForm" class="up white scroll_link">
+                            <?=__('[:ru]Контакты[:en]Contacts');?>
+                        </a></li>
                     <li>
                         <div class="social_block">
                             <ul>
