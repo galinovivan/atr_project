0653302ec1ee107685cc898ec947d2cb8d18e4ee
galinovivan <9225913@gmail.com>
2017-05-25 15:52:52 +0300

    s

--- gallery-students.php
+++ gallery-students.php
@@ -12,6 +12,9 @@
 
 <main>
         <div class="category_heading">
+            <a href="<?=$_SERVER['HTTP_REFERER'];?>" class="heading_back_link">
+                <i class="fa fa-arrow-left"></i>
+            </a>
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
