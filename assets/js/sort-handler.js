651f2a1258b1a5ba1d6102a2391ac13ae56ea749
galinovivan <9225913@gmail.com>
2017-05-29 13:17:38 +0300

    sort

---
+++ assets/js/sort-handler.js
@@ -0,0 +1,39 @@
+/**
+ * Created by root on 29.05.17.
+ */
+'use strict';
+(function($) {
+
+
+$(document).ready(function() {
+
+      var sortField = $('.sort_field');
+      var galleryList = $('.gallery_list');
+      sortField.click(function(event) {
+          event.preventDefault();
+          var sortType = $(this).attr('data-sort');
+          var terms = $(this).attr('data-terms');
+          var responseData = {
+              'action': 'myfilter',
+              'sort_type': sortType,
+              'terms': terms
+          };
+          $.ajax({
+              url: window.wp_data.ajax_url,
+              data: responseData,
+              type: 'POST',
+              beforeSend: function (xhr) {
+                  galleryList.find('.sort_preloader').show();
+              },
+              success: function (data) {
+                  galleryList.find('.sort_preloader').hide();
+                  galleryList.html(data);
+              }
+          })
+
+      });
+
+  })
+
+
+})(jQuery);
