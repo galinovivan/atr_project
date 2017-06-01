c8f64e5e0b997b3f4a9462314fdcdd41e84e5b09
galinovivan <9225913@gmail.com>
2017-06-01 14:29:31 +0300

    s

--- template-parts/registration.php
+++ template-parts/registration.php
@@ -11,33 +11,12 @@
 
 <div class="registration auth_form">
     <div class="form_description text-center">
-        <p class="black">Пожалуйста, зарегистрируйтесь, чтобы
-            принять участие в конкурсе.</p>
+        <p class="black">
+            <?=lang('please register to take part in a competition', true);?>.
+        </p>
         
     </div>
     <div class="reg_form">
-<!--        <form class="base_form userform" method="post" action="">-->
-<!--            <div class="row">-->
-<!--                <div class="col-md-12 field_item">-->
-<!--                    <input type="text" name="user_name" placeholder="Имя Фамилия" required />-->
-<!--                </div>-->
-<!--                <div class="col-md-12 field_item">-->
-<!--                    <input type="email" name="user_email" placeholder="Email" required />-->
-<!--                </div>-->
-<!--                <div class="col-md-12 checkbox_field">-->
-<!--                    <input type="checkbox" name="confirm" id="authrConfirm" required/>-->
-<!--                    <label for="authrConfirm">Я согласен на обработку своих персональных данных</label>-->
-<!--                </div>-->
-<!--                <div class="col-md-12 submit_field">-->
-<!--                    <input type="submit" class="button base medium black block_center" placeholder="Регистрация"/>-->
-<!--                </div>-->
-<!--                <input type="hidden" name="redirect_to" value="--><?//=$_SERVER['REQUEST_URI'];?><!--"/>-->
-<!--                <input type="hidden" name="nonce" value="--><?//=wp_create_nonce('register_me_nonce');?><!--"/>-->
-<!--                <input type="hidden" name="action" value="register_me" />-->
-<!--                <input type="hidden" name="step_success" value="#infoRegistration" id="stepSuccess"/>-->
-<!--                <div class="response black"></div>-->
-<!--            </div>-->
-<!--        </form>-->
         <?=do_shortcode('[wpuf_profile type="registration" id="348"]')?>
     </div>
 </div>
