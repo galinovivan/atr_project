2825a6729f029dd0679da28976011dae1b1bdab2
galinovivan <9225913@gmail.com>
2017-06-01 12:08:05 +0300

    s

--- index.php
+++ index.php
@@ -507,9 +507,9 @@
             </div>
             <div class="col-lg-8 col-md-offset-2 col-md-9 contact_form_wrapper">
                 <div class="contact_form">
-<!--                   <?php if ($curerntLang == 'ru'): ?>
+<!--                   <?php if ($currentLang == 'ru'): ?>
                     <?= do_shortcode('[contact-form-7 id="431" title="Контактная форма на главной"]'); ?>
-                    <?php elseif ($curerntLang == 'en'): ?>
+                    <?php elseif ($currentLang == 'en'): ?>
                     <?= do_shortcode('[contact-form-7 id="644" title="eng_contact_form"]'); ?>
                     <?php endif; ?>
             </div>
