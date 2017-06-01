d43292e34c3d7c83a0f3c2bf1be9ace17ef3556c
galinovivan <9225913@gmail.com>
2017-06-01 16:15:13 +0300

    add translation

--- languages/18n_en.php
+++ languages/18n_en.php
@@ -110,5 +110,6 @@ return [
     'please register to take part in a competition' => 'please register to take part in a competition',
     'if you have already registered, sign in, so that we can recognize you' => 'if you have already registered, sign in, so that we can recognize you',
     'registration on the site' => 'registration on the site',
+    'registration before template' => 'to download the template you need to register',
 
 ];
