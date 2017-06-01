4940bc4fdfd78af10a639b700a7866cba67b25af
galinovivan <9225913@gmail.com>
2017-06-01 11:30:23 +0300

    ya

--- assets/js/yandex.js
+++ assets/js/yandex.js
@@ -7,16 +7,60 @@
 
         var topHelperButton = $('#topActionForUnloggined');
         topHelperButton.on('click', function (event) {
-            event.preventDefault();
             sendDataToYandexMetrica('topHelperHereButton');
         });
 
 
-
-    function sendDataToYandexMetrica(eventId) {
-        yaCounter44245474.reachGoal(eventId, function() {
-          console.log('Send event data to ' + eventId);
+        var registrationForm = $('.reg_form').find('form');
+        registrationForm.submit(function() {
+            sendDataToYandexMetrica('registrationForm');
         });
-    }
+
+        var buttonLearnMore = $('#buttonLearnMore');
+        buttonLearnMore.click(function() {
+           sendDataToYandexMetrica('buttonLearnMore');
+        });
+
+        var buttomRegistrationButton = $('#buttonRegistrationOnButtom');
+        buttomRegistrationButton.click(function() {
+           sendDataToYandexMetrica('bottomRegistration');
+        });
+
+        var bottomContactForm = $('.wpcf7-form');
+
+        bottomContactForm.click(function() {
+           sendDataToYandexMetrica('bottomContactForm');
+        });
+
+
+        var houseNominationButton = $('#buttonHouse');
+        houseNominationButton.click(function() {
+           sendDataToYandexMetrica('buttonHouse');
+        });
+
+        var cityOnTheWaterNomination = $('#ButtonCityOntheWater');
+        cityOnTheWaterNomination.click(function() {
+           sendDataToYandexMetrica('cityOnTheWaterNomination');
+        });
+
+        var appartamentsNomination = $('#buttonAppartaments');
+        appartamentsNomination.click(function() {
+           sendDataToYandexMetrica('buttonApartamentsNomination');
+        });
+
+        var buttonBusStation = $('#buttonBusStation');
+        buttonBusStation.click(function() {
+           sendDataToYandexMetrica('buttonBusStationNomination');
+        });
+
+        /**
+         *
+          * @param eventId
+         */
+        function sendDataToYandexMetrica(eventId) {
+             yaCounter44245474.reachGoal(eventId, function() {
+             console.log('Send event data to ' + eventId);
+             });
+        }
     });
 })(jQuery);
