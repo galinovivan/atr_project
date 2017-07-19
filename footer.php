bdd9e8ea15b58bcec85f217cbc71588833dec92e
galinovivan <9225913@gmail.com>
2017-07-19 17:31:08 +0300

    add ga id

--- footer.php
+++ footer.php
@@ -256,6 +256,16 @@
     } else { f(); }
     })(document, window, "yandex_metrika_callbacks");
 </script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-81026057-5', 'auto');
+    ga('send', 'pageview');
+
+</script>
 <noscript><div><img src="https://mc.yandex.ru/watch/44245474" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 </body>
